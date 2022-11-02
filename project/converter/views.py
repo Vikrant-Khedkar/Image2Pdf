2693a2c1a071b8f0d8fc1fdbd8d6d45583e650bb
Vikrant Khedkar <vikrantkhedkar2720@gmail.com>
2022-11-02 19:58:06 +0530

    Added converting logic

--- project/converter/views.py
+++ project/converter/views.py
@@ -2,34 +2,39 @@ from email.mime import image
 from multiprocessing import context
 from urllib import request
 from django import forms, views
+from PIL import Image
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import viewsets
 from django import forms
-from .models import  ImageData
+from .models import ImageData
 from . import serializers
 from rest_framework.response import Response
+import img2pdf
 # Create your views here.
 
+
 def uploadimage(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
 
 def test(request):
     data = ImageData.objects.latest('id')
+    base = 'C:\Developer\Django practice\ImageToPdf\project'
+    url = data.image.url
+    link = base+url
+    image_1 = Image.open(link)
+    im_1 = image_1.convert('RGB')
+    pdf = im_1.save(r'C:\Developer\Django practice\ImageToPdf\project\media\converted.pdf')
     context = {
-        'data' : data
+        'data': data,
+        'pdf':pdf
     }
-    return render(request,"display.html", context)
+
+    return render(request, "display.html", context)
+
 
 class ConverterApiView(viewsets.ModelViewSet):
     serializer_class = serializers.Converter
     queryset = ImageData.objects.all()
-
-    
-
-
-
-
-
-
